80a4d86e4ebc933c5de458989bb99cf3912f99bd
jamel-perch <jamel.cole@perch.com>
2022-11-16 00:45:11 -0500

    update exports; update package.json

---
+++ lib/index.ts
@@ -0,0 +1,7 @@
+export * from "./useByproduct";
+export * from "./useEffect";
+export * from "./useEvent";
+export * from "./useProduct";
+export * from "./useShared";
+export * from "./useState";
+export * from "./useUpdate";
